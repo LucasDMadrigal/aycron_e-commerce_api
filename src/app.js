0f488d3e0d089770f94c9ae398d0667834d6d992
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-09-10 14:11:32 -0300

    iniciando controladores!

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ProductRouter from "./routes/products.router.js";
 import CartsRouter from "./routes/carts.router.js";
+import connectDB from "./config/db.js";
 
 const app = express();
 app.use(express.json());
@@ -10,11 +11,12 @@ app.use(express.urlencoded({ extended: true }));
  * importar productos del archivo products.json
  */
 
+connectDB();
 
 app.use("/products", ProductRouter);
 
 app.use("/carts", CartsRouter);
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server started on port 8080");
 });
--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -13,4 +15,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB
