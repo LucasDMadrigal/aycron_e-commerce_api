f2e4f6910b5c17ab6ff8681d3b764f47a5d81cf2
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-09-22 20:15:08 -0300

    creacion del midd admin para creacion de prod

---
+++ src/infrastructure/webserver/middleware/adminMiddleware.js
@@ -0,0 +1,8 @@
+export const adminMiddleware = (req, res, next) => {
+    // Verificar si el usuario está autenticado y tiene rol de admin
+    if (req.user && req.user.isAdmin) {
+      next(); // El usuario es admin, continuar con la ejecución
+    } else {
+      return res.status(403).json({ message: 'Access denied, admin only' });
+    }
+  };
