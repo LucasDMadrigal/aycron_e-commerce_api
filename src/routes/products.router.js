34b3c39abdb293477ac9b1414d79d88e6c11460d
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-09-25 15:21:01 -0300

    correcciones auth

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import productController from "../controllers/product.controller.js"
+import authMiddleware from "../middlewares/auth.middleware.js";
+import adminMiddleware from "../middlewares/admin.middleware.js";
 
 const router = Router();
 
-router.get("/", productController.getProducts);
-router.get("/:id", productController.getProductById);
-router.post("/", productController.createProduct);
-router.delete("/:id", productController.deleteProduct);
-router.put("/:id", productController.updateProduct);
+router.get("/", authMiddleware, productController.getProducts);
+router.get("/:id", authMiddleware, productController.getProductById);
+router.post("/", authMiddleware, adminMiddleware, productController.createProduct);
+router.delete("/:id", authMiddleware, adminMiddleware, productController.deleteProduct);
+router.put("/:id", authMiddleware, adminMiddleware, productController.updateProduct);
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller.js"
+import authMiddleware from "../middlewares/auth.middleware.js";
+import adminMiddleware from "../middlewares/admin.middleware.js";
+
 const router = Router();
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
-router.get("/", userController.getUsers);
-router.get("/:id", userController.getUserById);
-router.delete("/:id", userController.deleteUser);
-router.put("/:id", userController.updateUser);
+router.get("/", authMiddleware, adminMiddleware, userController.getUsers);
+router.get("/:id", authMiddleware, userController.getUserById);
+router.delete("/:id", authMiddleware, adminMiddleware, userController.deleteUser);
+router.put("/:id", authMiddleware, userController.updateUser);
 export default router;
